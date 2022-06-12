ab76d3e89eb709c537b8d99a94a650c5ff80b40d
EmanuelAugusto <emanuel02augusto@gmail.com>
2022-06-11 23:25:47 -0300

    added rective

--- components/components.js
+++ components/components.js
@@ -1,9 +1,52 @@
-export const App = (app, rootElement, title) => {
+export const App = (app, rootElement, title, data, UiText) => {
     if (title) document.title = title;
+
     document.getElementById(rootElement).appendChild(app);
+    RequestRenderUI(data, UiText);
 };
 
-export const Input = ({ placeholder, type, id, name, onInput }) => {
+export const RequestRenderUI = (data, UiText) => {
+    const elments = document.querySelectorAll("[reactive]");
+
+    for (const el of elments) {
+        const attrVal = el.getAttribute("bind");
+        const uiText = el.getAttribute("uiText");
+
+        if (["input"].includes(el.tagName.toLocaleLowerCase())) {
+            const [directive, bindVar] = attrVal.split(":");
+
+            el.value = data[bindVar];
+        } else {
+            const valueSplited = attrVal.split("|");
+
+            let text = "";
+
+            for (const binds of valueSplited) {
+                const [directive, bindVar] = binds.split(":");
+
+                if (text) {
+                    text = text.replaceAll("${" + bindVar + "}", data[bindVar]);
+                } else {
+                    text = UiText[uiText].replaceAll("${" + bindVar + "}", data[bindVar]);
+                }
+            }
+
+            el.textContent = text;
+        }
+    }
+};
+
+export const Input = ({
+    placeholder,
+    type,
+    id,
+    name,
+    onInput,
+    classList,
+    reactive,
+    bind,
+    bindUiText,
+}) => {
     const input = document.createElement("input");
     input.type = type;
     if (id) input.id = id;
@@ -19,15 +62,35 @@ export const Input = ({ placeholder, type, id, name, onInput }) => {
         };
     }
 
+    if (classList) {
+        for (const className of classList) {
+            input.classList.add(className);
+        }
+    }
+
+    if (reactive) {
+        input.setAttribute("reactive", "");
+    }
+
+    if (bind) input.setAttribute("bind", bind);
+
+    if (bindUiText) p.setAttribute("uiText", bindUiText);
+
     return input;
 };
 
-export const Div = ({ textContent, childs, id }) => {
+export const Div = ({ textContent, childs, id, classList }) => {
     const div = document.createElement("div");
     if (id) div.id = id;
 
     if (textContent) {
         div.textContent = textContent;
+    }
+
+    if (classList) {
+        for (const className of classList) {
+            div.classList.add(className);
+        }
     }
 
     if (childs) {
@@ -39,45 +102,104 @@ export const Div = ({ textContent, childs, id }) => {
     return div;
 };
 
-export const Checkbox = ({ value, id, name }) => {
+export const Checkbox = ({
+    value,
+    id,
+    name,
+    classList,
+    onClick,
+    reactive,
+    bind,
+    bindUiText,
+}) => {
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+
     checkbox.value = value;
+
     if (id) checkbox.id = id;
     if (name) checkbox.name = name;
+
+    if (onClick) {
+        checkbox.onclick = () => {
+            onClick(checkbox);
+        };
+    }
+
+    if (classList) {
+        for (const className of classList) {
+            checkbox.classList.add(className);
+        }
+    }
+
+    if (reactive) {
+        checkbox.setAttribute("reactive", "");
+    }
+
+    if (bind) checkbox.setAttribute("bind", bind);
+
+    if (bindUiText) checkbox.setAttribute("uiText", bindUiText);
 
     return checkbox;
 };
 
-export const Radio = ({ value, id, name }) => {
+export const Radio = ({ value, id, name, classList }) => {
     const radio = document.createElement("input");
     radio.type = "radio";
     radio.value = value;
     if (id) radio.id = id;
     if (name) radio.name = name;
 
+    if (classList) {
+        for (const className of classList) {
+            radio.classList.add(className);
+        }
+    }
+
     return radio;
 };
 
-export const Label = ({ labelFor, textContent, id }) => {
+export const Label = ({ labelFor, textContent, id, classList }) => {
     const label = document.createElement("label");
     label.for = labelFor;
     label.textContent = textContent;
     if (id) label.id = id;
 
+    if (classList) {
+        for (const className of classList) {
+            label.classList.add(className);
+        }
+    }
+
     return label;
 };
 
-export const Text = ({ textContent, id }) => {
+export const Text = ({
+    textContent,
+    id,
+    classList,
+    bind,
+    bindUiText,
+    reactive,
+}) => {
     const p = document.createElement("p");
-
-    p.textContent = textContent;
+    if (textContent) p.textContent = textContent;
     if (id) p.id = id;
+
+    if (classList) {
+        for (const className of classList) {
+            p.classList.add(className);
+        }
+    }
+
+    if (reactive) p.setAttribute("reactive", "");
+    if (bind) p.setAttribute("bind", bind);
+    if (bindUiText) p.setAttribute("uiText", bindUiText);
 
     return p;
 };
 
-export const Form = ({ action, method, childs, onSubmit }) => {
+export const Form = ({ action, method, childs, onSubmit, classList }) => {
     const form = document.createElement("form");
     if (action) form.action = action;
 
@@ -94,10 +216,24 @@ export const Form = ({ action, method, childs, onSubmit }) => {
         }
     }
 
+    if (classList) {
+        for (const className of classList) {
+            form.classList.add(className);
+        }
+    }
+
     return form;
 };
 
-export const Button = ({ id, textContent, type, onClick }) => {
+export const Button = ({
+    id,
+    textContent,
+    type,
+    onClick,
+    classList,
+    bind,
+    bindUiText,
+}) => {
     const button = document.createElement("button");
     button.textContent = textContent;
     if (id) p.id = id;
@@ -112,10 +248,16 @@ export const Button = ({ id, textContent, type, onClick }) => {
         };
     }
 
+    if (classList) {
+        for (const className of classList) {
+            button.classList.add(className);
+        }
+    }
+
     return button;
 };
 
-export const Dialog = ({ childs, id }) => {
+export const Dialog = ({ childs, id, classList }) => {
     const dialog = document.createElement("dialog");
 
     if (!id) throw new Error("provid id to this component");
@@ -128,6 +270,12 @@ export const Dialog = ({ childs, id }) => {
         }
     }
 
+    if (classList) {
+        for (const className of classList) {
+            dialog.classList.add(className);
+        }
+    }
+
     return dialog;
 };
 
@@ -135,4 +283,27 @@ export const ShowModal = ({ name, state }) => {
     let stateOfModal = state ? "show" : "close";
 
     document.getElementById(name)[stateOfModal]();
+};
+
+export const WatchValue = (obj) => {
+    let value = obj.foo;
+
+    obj.registerNewListener((val) => {
+        value = val;
+    });
+
+    return value;
+};
+
+export const CreateStore = (obj, UiText) => {
+    return new Proxy(obj, {
+        get(target, property) {
+            return target[property];
+        },
+        set(target, property, value) {
+            target[property] = value;
+            RequestRenderUI(obj, UiText);
+            return true;
+        },
+    });
 };
--- main.js
+++ main.js
@@ -10,20 +10,47 @@ import {
     Button,
     Dialog,
     ShowModal,
+    CreateStore,
 } from "./components/components.js";
+import "./style.css";
 
-let variableWithName = "Ola mundo";
-
-const onInput = (value) => {
-    variableWithName = value.value;
-    document.getElementById("textInput").textContent = value.value;
+const UiText = {
+    age: "",
+    primaryText: "Hoje é ${bar} Meu nome é ${foo}, minha idade é: ${age} e minhas comidas favoritas são: ${macarrao} ${camarao}",
 };
 
-let stateModal = false;
+const obj = CreateStore({
+        foo: "",
+        bar: new Date().toISOString(),
+        age: "",
+        macarrao: "",
+        camarao: "",
+    },
+    UiText
+);
 
-const onClick = () => {
-    stateModal = !stateModal;
-    ShowModal({ name: "modalOne", state: stateModal });
+const onInput = (value) => {
+    obj.foo = value.value;
+};
+
+const onInputAge = (value) => {
+    obj.age = value.value;
+};
+
+const onInputFood = (value) => {
+    if (value.checked) {
+        obj.macarrao = "macarrao";
+    } else {
+        obj.macarrao = "";
+    }
+};
+
+const onInputCamarao = (value) => {
+    if (value.checked) {
+        obj.camarao = "camarao";
+    } else {
+        obj.camarao = "";
+    }
 };
 
 const onSubmit = (evt) => {
@@ -36,34 +63,83 @@ const AppComposition = Div({
         Form({
             childs: [
                 Input({
+                    classList: ["input-border", "margin-bt"],
                     placeholder: "Qual seu nome?",
                     type: "text",
+                    reactive: true,
+                    bind: "value:foo",
                     onInput: onInput,
                 }),
-                Button({ textContent: "teste", type: "submit" }),
+                Input({
+                    classList: ["input-border", "margin-bt"],
+                    placeholder: "Qual sua idade?",
+                    type: "number",
+                    reactive: true,
+                    bind: "value:age",
+                    onInput: onInputAge,
+                }),
+                Button({
+                    classList: ["button", "cursor-pointer", "margin-bt"],
+                    textContent: "Enviar",
+                    type: "submit",
+                }),
             ],
             onSubmit: onSubmit,
+            classList: ["margin-bt", "display-column", "display-flex"],
         }),
-        Button({ textContent: "Abrir modal", onClick: onClick }),
-        Input({ placeholder: "Qual seu nome?", type: "text" }),
-        Input({ placeholder: "Qual sua idada?", type: "text" }),
+        Div({
+            classList: [
+                "bg-red",
+                "display-flex",
+                "margin-bt",
+                "text-white",
+                "padding-05",
+            ],
+            childs: [
+                Text({
+                    bindUiText: "primaryText",
+                    bind: "value:foo|value:bar|value:age|value:macarrao|value:camarao",
+                    reactive: true,
+                }),
+            ],
+        }),
         Div({
             childs: [
-                Dialog({
-                    id: "modalOne",
-                    childs: [Input({ placeholder: "Qual seu nome?", type: "text" })],
+                Input({
+                    classList: ["input-border"],
+                    placeholder: "Qual seu nome?",
+                    type: "text",
+                    reactive: true,
+                    bind: "value:foo",
                 }),
-                Text({ textContent: variableWithName, id: "textInput" }),
-                Label({ labelFor: "teste1", textContent: "Você é maior de idade?" }),
-                Checkbox({ value: "teste", id: "teste1" }),
-                Text({ textContent: "Você concorda com os termos?" }),
-                Label({ labelFor: "teste2", textContent: "Sim" }),
-                Radio({ value: "teste2", id: "teste2" }),
-                Label({ labelFor: "teste3", textContent: "Não" }),
-                Radio({ value: "teste3", id: "teste2" }),
+                Div({
+                    childs: [
+                        Text({ textContent: "Que comidas você gosta?" }),
+                        Checkbox({
+                            value: "macarrao",
+                            id: "checkOne",
+                            reactive: true,
+                            bind: "value:macarrao",
+                            onClick: onInputFood,
+                        }),
+                        Label({ labelFor: "checkOne", textContent: "macarrão" }),
+                        Checkbox({
+                            value: "camarao",
+                            id: "checkTwo",
+                            reactive: true,
+                            bind: "value:camarao",
+                            onClick: onInputCamarao,
+                        }),
+                        Label({ labelFor: "checkTwo", textContent: "Camarão" }),
+                    ],
+                    classList: ["display-flex", "align-items"],
+                }),
             ],
+        }),
+        Text({
+            textContent: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         }),
     ],
 });
 
-App(AppComposition, "app", "Minha aplicação");
+App(AppComposition, "app", "Minha aplicação", obj, UiText);
